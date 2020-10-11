392d7d4d904e041bcbc655fe7e91c8ea6156209d
Ali Jamali <jamaliali219@gmail.com>
2020-10-11 17:43:58 +0330

    Use DataBinding in MainActivity

--- app/src/main/java/ir/anexception/rogheh/MainActivity.kt
+++ app/src/main/java/ir/anexception/rogheh/MainActivity.kt
@@ -3,11 +3,13 @@ package ir.anexception.rogheh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.databinding.DataBindingUtil
+import ir.anexception.rogheh.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL;
     }
 }
