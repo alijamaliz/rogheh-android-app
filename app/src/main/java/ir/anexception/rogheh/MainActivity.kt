595e9ddd8cb0bdde51a160f2573fd22d962cd214
Ali Jamali <jamaliali219@gmail.com>
2020-10-11 16:30:23 +0330

    Force MainActivity to launch in RTL direction

--- app/src/main/java/ir/anexception/rogheh/MainActivity.kt
+++ app/src/main/java/ir/anexception/rogheh/MainActivity.kt
@@ -2,10 +2,12 @@ package ir.anexception.rogheh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL;
     }
 }
