44b589a05c64f002878e488c1adab62528652547
Ali Jamali <jamaliali219@gmail.com>
2020-10-11 17:50:25 +0330

    Setup action bar with navigation controller in MainActivity

--- app/src/main/java/ir/anexception/rogheh/MainActivity.kt
+++ app/src/main/java/ir/anexception/rogheh/MainActivity.kt
@@ -4,12 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import ir.anexception.rogheh.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        navController = this.findNavController(R.id.notesNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
         window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL;
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
