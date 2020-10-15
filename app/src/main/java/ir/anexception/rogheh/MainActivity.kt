5944d6b17312452860c97743fc61f2e63067e57d
Ali Jamali <jamaliali219@gmail.com>
2020-10-15 13:10:50 +0330

    Implement basic navigation drawer in Main activity

--- app/src/main/java/ir/anexception/rogheh/MainActivity.kt
+++ app/src/main/java/ir/anexception/rogheh/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -12,16 +13,21 @@ import ir.anexception.rogheh.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        navController = this.findNavController(R.id.notesNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+
         window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL;
+
+        navController = this.findNavController(R.id.notesNavHostFragment)
+        drawerLayout = binding.drawerLayout
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
