014be58d386af6fa3a70e79fe9be92e615999c7c
Ali Jamali <jamaliali219@gmail.com>
2020-10-15 10:48:57 +0330

    Add simple useless menu in NoteFragment

--- app/src/main/java/ir/anexception/rogheh/NoteFragment.kt
+++ app/src/main/java/ir/anexception/rogheh/NoteFragment.kt
@@ -1,10 +1,9 @@
 package ir.anexception.rogheh
 
 import android.os.Bundle
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import ir.anexception.rogheh.databinding.FragmentNoteBinding
 
@@ -16,7 +15,27 @@ class NoteFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentNoteBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_note, container, false)
+
+        setHasOptionsMenu(true);
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.note_menu, menu);
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.copyNote -> {
+                Log.d("SHALGHAM", "FARANGI")
+                return true
+            }
+            R.id.editNote -> false
+            R.id.removeNote -> false
+            else -> false
+        }
     }
 
 }
